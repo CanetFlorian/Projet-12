8e7f772317ad794e8bd58f739a28892203729d76
Canet Florian <florian.canet12@gmail.com>
2025-04-07 18:30:06 +0200

    Ajout projet/form

--- portfolio/src/components/Contact/index.jsx
+++ portfolio/src/components/Contact/index.jsx
@@ -1,10 +1,146 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import {init, sendForm} from '@emailjs/browser';
 
+const ContactContainer = styled.section`
+  padding: 80px 20px;
+  background-color: #f7fafc;
+  text-align: center;
+
+  h2 {
+    font-size: 2.5rem;
+    margin-bottom: 40px;
+    color: #333;
+  }
+`;
+
+const FormWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+  background: white;
+  padding: 30px;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+const InputField = styled.input`
+  width: 100%;
+  padding: 12px;
+  margin: 10px 0;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+  font-size: 1rem;
+
+  &:focus {
+    border-color: #007bff;
+    outline: none;
+  }
+`;
+
+const TextAreaField = styled.textarea`
+  width: 100%;
+  padding: 12px;
+  margin: 10px 0;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+  font-size: 1rem;
+  height: 150px;
+
+  &:focus {
+    border-color: #007bff;
+    outline: none;
+  }
+`;
+
+const SubmitButton = styled.button`
+  padding: 10px 20px;
+  border: none;
+  background-color: #007bff;
+  color: white;
+  font-size: 1rem;
+  cursor: pointer;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
 
 const Contact = () => {
-return (
-    <h2>Contact</h2>
-);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  useEffect(()=> {
+    init('iLv-cGVEhWWQ_7MP3');
+  },[]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sendForm('service_32v7xai', 'template_kb9knoq', e.target, 'iLv-cGVEhWWQ_7MP3')
+        .then(
+      (result) => {
+        console.log('Email envoyé : ', result.text);
+       
+      },
+      (error) => {
+        console.log('Erreur d\'envoi : ', error.text);
+        alert('Désolé, il y a eu un problème avec l\'envoi de votre message. Essayez à nouveau plus tard.');
+      }
+    );
+    setFormData({ name: '', email: '', message: '' });
+    alert('Merci pour votre message!');
+  };
+
+  return (
+    <ContactContainer id="contact">
+      <h2>Contactez-moi</h2>
+      <FormWrapper>
+        <form onSubmit={handleSubmit}>
+          <InputField
+            type="text"
+            name="name"
+            placeholder="Votre nom"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+          <InputField
+            type="email"
+            name="email"
+            placeholder="Votre email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          <TextAreaField
+            name="message"
+            placeholder="Votre message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          />
+          <SubmitButton type="submit">Envoyer</SubmitButton>
+        </form>
+      </FormWrapper>
+    </ContactContainer>
+  );
 };
 
 export default Contact;
--- portfolio/src/components/Projets/index.jsx
+++ portfolio/src/components/Projets/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import ohmyfood from '../../assets/ohmyfood.png';
+import kasa from '../../assets/kasa.png';
+import sophiebluel from '../../assets/sophiebluel.png';
 
 const ProjectsContainer = styled.section`
   padding: 80px 20px;
@@ -7,27 +10,72 @@ const ProjectsContainer = styled.section`
   text-align: center;
 `;
 
+
 const ProjectCard = styled.div`
+  display: flex;
+  flex-direction: row; 
+  align-items: center;
+  justify-content: center;
   background: white;
   border-radius: 8px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   margin: 20px;
   padding: 20px;
+  min-height: 300px;
+  color: #333;
   transition: transform 0.3s ease;
-  
+
   &:hover {
     transform: translateY(-10px);
   }
+
+    @media (max-width: 768px) {
+    flex-direction: column;
+    align-items:center;}
+`;
+
+
+const ImageContainer = styled.div`
+  width: 40%;
+  padding-right: 20px; 
+
+  @media (max-width:768px) {
+  width:80%;
+  padding-right:0;}
+`;
+
+const ProjectImageStyled = styled.img`
+  width: 100%;
+  height: auto;
+  border-radius: 8px;
+`;
+
+const TextContainer = styled.div`
+  width: 60%;
+  text-align: left;
+
+  @media (max-width:768px) {
+  width:100%;
+  text-align: center;
+  margin-top:20px;}
 `;
 
 const ProjectTitle = styled.h3`
-  font-size: 1.5rem;
+  font-size: 1.8rem;
   margin-bottom: 10px;
+  color: #333;
+
+  @media (max-width:768px) {
+  font-size: 1.5rem;}
 `;
 
 const ProjectDescription = styled.p`
   font-size: 1rem;
   color: #555;
+  margin-bottom: 15px;
+
+  @media (max-width: 768px) {
+  font-size: 0.9rem;}
 `;
 
 const ProjectLink = styled.a`
@@ -35,10 +83,13 @@ const ProjectLink = styled.a`
   margin-top: 10px;
   color: #007bff;
   text-decoration: none;
-  
+
   &:hover {
     text-decoration: underline;
   }
+
+  @media (max-width:768px) {
+  font-size: 1rem;}
 `;
 
 const Projects = () => {
@@ -47,34 +98,49 @@ const Projects = () => {
       <h2>Mes Projets</h2>
       <div>
         <ProjectCard>
-          <ProjectTitle>Projet 1 - Ohmyfood</ProjectTitle>
-          <ProjectDescription>
-            En tant que développeur junior, j’ai intégré la startup Ohmyfood pour développer un site "mobile first"
-            répertoriant les menus de restaurants gastronomiques à Paris. Le site est conçu pour être responsive et
-            intègre des animations CSS soignées, permettant une expérience utilisateur fluide sur mobile, tablette et desktop.
-            Le projet utilise SASS pour organiser le CSS et s’appuie sur les technologies modernes du web.
-          </ProjectDescription>
-          <ProjectLink href="https://lien-vers-le-projet.com" target="_blank">Voir le projet</ProjectLink>
+            <ImageContainer>
+                <ProjectImageStyled src={ohmyfood} alt="Photo du site OhmyFood"/>
+            </ImageContainer>
+            <TextContainer>
+                <ProjectTitle>Projet 1 - Ohmyfood</ProjectTitle>
+                <ProjectDescription>
+              En tant que développeur junior, j’ai intégré la startup Ohmyfood pour développer un site "mobile first"
+              répertoriant les menus de restaurants gastronomiques à Paris. Le site est conçu pour être responsive et
+              intègre des animations CSS soignées, permettant une expérience utilisateur fluide sur mobile, tablette et desktop.
+              Le projet utilise SASS pour organiser le CSS et s’appuie sur les technologies modernes du web.
+                </ProjectDescription>
+                <ProjectLink href="https://canetflorian.github.io/Projet-4-OhMyFood/" target="_blank">Voir le projet</ProjectLink>
+            </TextContainer>
         </ProjectCard>
 
         <ProjectCard>
-          <ProjectTitle>Projet 2 - Site Portfolio de Sophie Bluel</ProjectTitle>
-          <ProjectDescription>
-            En tant que développeur front-end pour l’agence ArchiWebos, j’ai contribué à la création du portfolio de l'architecte
-            d’intérieur Sophie Bluel. Mes principales missions incluaient la création de la page de présentation des travaux,
-            la page de connexion de l'administrateur et le développement d’une modale pour l’upload de nouveaux médias.
-          </ProjectDescription>
-          <ProjectLink href="https://lien-vers-le-projet.com" target="_blank">Voir le projet</ProjectLink>
+            <ImageContainer>
+                <ProjectImageStyled src={sophiebluel} alt="Photo du site de Sophie Bluel"/>
+            </ImageContainer>
+            <TextContainer>
+                <ProjectTitle>Projet 1 - Ohmyfood</ProjectTitle>
+                <ProjectDescription>
+                En tant que développeur front-end pour l’agence ArchiWebos, j’ai contribué à la création du portfolio de l'architecte
+              d’intérieur Sophie Bluel. Mes principales missions incluaient la création de la page de présentation des travaux,
+              la page de connexion de l'administrateur et le développement d’une modale pour l’upload de nouveaux médias.
+                </ProjectDescription>
+                <ProjectLink href="" target="_blank">Voir le projet</ProjectLink>
+            </TextContainer>
         </ProjectCard>
 
         <ProjectCard>
-          <ProjectTitle>Projet 3 - Refonte du site de Kasa</ProjectTitle>
-          <ProjectDescription>
-            En tant que développeur front-end freelance pour Kasa, j’ai participé à la refonte complète du site de location d’appartements.
-            Le projet impliquait de migrer le code legacy en ASP.NET vers une stack moderne JavaScript avec NodeJS côté back-end et React côté front-end.
-            Une nouvelle interface a été conçue pour améliorer l'expérience utilisateur et renforcer la performance du site.
-          </ProjectDescription>
-          <ProjectLink href="https://lien-vers-le-projet.com" target="_blank">Voir le projet</ProjectLink>
+            <ImageContainer>
+                <ProjectImageStyled src={kasa} alt="Photo du site de Kasa"/>
+            </ImageContainer>
+            <TextContainer>
+                <ProjectTitle>Projet 1 - Ohmyfood</ProjectTitle>
+                <ProjectDescription>
+                En tant que développeur front-end freelance pour Kasa, j’ai participé à la refonte complète du site de location d’appartements.
+              Le projet impliquait de migrer le code legacy en ASP.NET vers une stack moderne JavaScript avec NodeJS côté back-end et React côté front-end.
+              Une nouvelle interface a été conçue pour améliorer l'expérience utilisateur et renforcer la performance du site.
+                </ProjectDescription>
+                <ProjectLink href="" target="_blank">Voir le projet</ProjectLink>
+            </TextContainer>
         </ProjectCard>
       </div>
     </ProjectsContainer>
